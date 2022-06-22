d26f06a6f56f7d13a6b2ef927d3a1ff6ac24a82c
Aliaksei Bialenka <aliaksei.bialenka@innowise-group.com>
2022-06-22 17:48:47 +0300

    feat: header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+
 export default () => {
   return (
-    <>
-    !!!!
-      Container
-      <MarketingApp />
-    </>
+    <BrowserRouter>
+      <>
+        <Header />
+        <MarketingApp />
+      </>
+    </BrowserRouter>
   );
 };
