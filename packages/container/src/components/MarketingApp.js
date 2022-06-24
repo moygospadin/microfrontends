3da970c6aabfd00afb3d0c1580a48842c74e36b9
Aliaksei Bialenka <aliaksei.bialenka@innowise-group.com>
2022-06-24 16:23:57 +0300

    pipeline: rerun

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,16 +1,19 @@
-import React, { useRef, useEffect } from "react";
-
 import { mount } from "marketing/MarketingApp";
+import React, { useRef, useEffect } from "react";
 import { useHistory } from "react-router-dom";
+
 export default () => {
   const ref = useRef(null);
   const history = useHistory();
+
   useEffect(() => {
-    
     const { onParentNavigate } = mount(ref.current, {
       onNavigate: ({ pathname: nextPathname }) => {
         const { pathname } = history.location;
-        pathname !== nextPathname && history.push(nextPathname);
+
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
       },
     });
 
